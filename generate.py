8f072aaeee5bacaf3b35cc035b1badcc7f90c9ff
Guillaume Meigniez <gmeigniez@manadge.co>
2021-03-28 20:30:04 +0200

    [LOG] Add log to see generate input dim 2.

--- generate.py
+++ generate.py
@@ -117,7 +117,7 @@ def generate_images(
         print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
         z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
         img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
-        print ('input dims', np.random.RandomState(seed).randn(1, G.z_dim))
+        print ('input dims', np.random.RandomState(seed).randn(1, G.z_dim).size, np.random.RandomState(seed).randn(1, G.z_dim)[0].size)
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
         PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
 
