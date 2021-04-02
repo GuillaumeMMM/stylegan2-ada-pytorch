8c252dd213bde0257aeae1b9f98949e80688935f
Guillaume Meigniez <gmeigniez@manadge.co>
2021-04-02 21:50:25 +0200

    [GENERATE] Add array. 5

--- generate.py
+++ generate.py
@@ -124,14 +124,14 @@ def generate_images(
     #    PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
 
     print('Generating image for seed custom')
-    z = torch.from_numpy(np.array(position)).to(device)
-    img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
+    z = torch.from_numpy(np.array([position])).to(device)
+    """ img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
     img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-    PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed_custom.png')
+    PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed_custom.png') """
 
 #----------------------------------------------------------------------------
 
-if __name__ == "__main__":
-    generate_images() # pylint: disable=no-value-for-parameter
+""" if __name__ == "__main__":
+    generate_images() # pylint: disable=no-value-for-parameter """
 
 #----------------------------------------------------------------------------
