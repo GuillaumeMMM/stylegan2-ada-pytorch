52866917439f65e70a61163db80ff816721f8b5c
Guillaume Meigniez <gmeigniez@manadge.co>
2021-04-02 19:59:23 +0200

    [GENERATE] Add array. 4

--- generate.py
+++ generate.py
@@ -124,7 +124,7 @@ def generate_images(
     #    PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
 
     print('Generating image for seed custom')
-    z = torch.from_numpy(np.array([position])).to(device)
+    z = torch.from_numpy(np.array(position)).to(device)
     img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
     img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
     PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed_custom.png')
